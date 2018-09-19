0ef9c6e59affd9b95b8e727940f602f9f84dcc66
Zach Albia <zach.albia@gmail.com>
2018-09-19 20:23:53 +0800

    feat(server): Implement schema

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,85 @@
-import { prisma } from "./prisma-client";
+import { GraphQLServer } from "graphql-yoga";
+import { IResolverObject, IResolvers } from "graphql-yoga/dist/types";
+import { Prisma, prisma } from "./prisma-client";
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createTodo({ title: "Alice" });
-  // tslint:disable:no-console
-  console.log(`Created new user: ${newUser.title} (ID: ${newUser.id})`);
-
-  // Read all users from the database and print them to the console
-  const allTodos = await prisma.todoes();
-  console.log(allTodos);
+interface Context {
+  prisma: Prisma;
 }
 
-main().catch(e => console.error(e));
+const Mutation: IResolverObject = {
+  createTodo(root, args: { title: string }, context: Context) {
+    return context.prisma.createTodo({
+      completed: false,
+      title: args.title
+    });
+  },
+  markTodoCompleted(root, args: { todoID: string }, context: Context) {
+    return context.prisma.updateTodo({
+      data: {
+        completed: true
+      },
+      where: {
+        id: args.todoID
+      }
+    });
+  },
+  changeTodoTitle(
+    root,
+    args: { todoID: string; title: string },
+    context: Context
+  ) {
+    return context.prisma.updateTodo({
+      data: {
+        title: args.title
+      },
+      where: {
+        id: args.todoID
+      }
+    });
+  },
+  deleteTodo(root, args: { todoID: string }, context: Context) {
+    return context.prisma.deleteTodo({
+      id: args.todoID
+    });
+  }
+};
+
+const Query: IResolverObject = {
+  todo(root, args: { todoID: string }, context: Context) {
+    return context.prisma.todo({
+      id: args.todoID
+    });
+  },
+  searchTodo(root, args: { needle: string }, context: Context) {
+    return context.prisma.todoes({
+      where: {
+        title_contains: args.needle
+      }
+    });
+  },
+  completedTodos(root, args, context: Context) {
+    return context.prisma.todoes({
+      where: {
+        completed: true
+      }
+    });
+  }
+};
+
+const resolvers: IResolvers = {
+  Mutation,
+  Query
+};
+
+const server = new GraphQLServer({
+  context: {
+    prisma
+  },
+  resolvers,
+  typeDefs: "../../schema.graphql"
+});
+
+// tslint:disable:no-console
+server
+  .start(() => console.log("Server is running on http://localhost:4000"))
+  .catch(console.error);
