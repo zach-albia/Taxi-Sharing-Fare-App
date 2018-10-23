af83298307521019632fdb689ac0703ccb6e1d2e
Zach Albia <zach.albia@gmail.com>
2018-10-23 14:08:39 +0800

    feat(client): Wrap Login and Registration forms in react-final-form Form

--- packages/client/src/components/pages/logout/LoginForm.tsx
+++ packages/client/src/components/pages/logout/LoginForm.tsx
@@ -5,37 +5,49 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import * as React from "react";
+import { Form } from "react-final-form";
 import { FormProps } from "../../../@types/common";
 
-export function LoginForm(props: FormProps) {
-  return (
-    <form className={props.classes.form}>
-      <FormControl margin="normal" required={true} fullWidth={true}>
-        <InputLabel htmlFor="email">Email Address</InputLabel>
-        <Input id="email" name="email" autoComplete="email" autoFocus={true} />
-      </FormControl>
-      <FormControl margin="normal" required={true} fullWidth={true}>
-        <InputLabel htmlFor="password">Password</InputLabel>
-        <Input
-          name="password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-        />
-      </FormControl>
-      <FormControlLabel
-        control={<Checkbox value="remember" color="primary" />}
-        label="Remember me"
-      />
-      <Button
-        type="submit"
-        fullWidth={true}
-        variant="contained"
-        color="primary"
-        className={props.classes.submit}
-      >
-        Sign in
-      </Button>
-    </form>
-  );
+export class LoginForm extends React.Component<FormProps> {
+  render() {
+    return (
+      <Form onSubmit={undefined}>
+        {() => (
+          <form className={this.props.classes.form}>
+            <FormControl margin="normal" required={true} fullWidth={true}>
+              <InputLabel htmlFor="email">Email Address</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus={true}
+              />
+            </FormControl>
+            <FormControl margin="normal" required={true} fullWidth={true}>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth={true}
+              variant="contained"
+              color="primary"
+              className={this.props.classes.submit}
+            >
+              Sign in
+            </Button>
+          </form>
+        )}
+      </Form>
+    );
+  }
 }
--- packages/client/src/components/pages/logout/RegistrationForm.tsx
+++ packages/client/src/components/pages/logout/RegistrationForm.tsx
@@ -3,32 +3,46 @@ import FormControl from "@material-ui/core/FormControl";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import * as React from "react";
+import { Form } from "react-final-form";
 import { FormProps } from "../../../@types/common";
 
 export function RegistrationForm(props: FormProps) {
   return (
-    <form className={props.classes.form}>
-      <FormControl margin="normal" required={true} fullWidth={true}>
-        <InputLabel htmlFor="email">Email Address</InputLabel>
-        <Input id="email" name="email" autoComplete="email" autoFocus={true} />
-      </FormControl>
-      <FormControl margin="normal" required={true} fullWidth={true}>
-        <InputLabel htmlFor="password">Password</InputLabel>
-        <Input name="password" type="password" id="password" />
-      </FormControl>
-      <FormControl margin="normal" required={true} fullWidth={true}>
-        <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-        <Input name="confirmPassword" type="password" id="confirmPassword" />
-      </FormControl>
-      <Button
-        type="submit"
-        fullWidth={true}
-        variant="contained"
-        color="primary"
-        className={props.classes.submit}
-      >
-        Sign in
-      </Button>
-    </form>
+    <Form onSubmit={undefined}>
+      {() => (
+        <form className={props.classes.form}>
+          <FormControl margin="normal" required={true} fullWidth={true}>
+            <InputLabel htmlFor="email">Email Address</InputLabel>
+            <Input
+              id="email"
+              name="email"
+              autoComplete="email"
+              autoFocus={true}
+            />
+          </FormControl>
+          <FormControl margin="normal" required={true} fullWidth={true}>
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input name="password" type="password" id="password" />
+          </FormControl>
+          <FormControl margin="normal" required={true} fullWidth={true}>
+            <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
+            <Input
+              name="confirmPassword"
+              type="password"
+              id="confirmPassword"
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth={true}
+            variant="contained"
+            color="primary"
+            className={props.classes.submit}
+          >
+            Sign in
+          </Button>
+        </form>
+      )}
+    </Form>
   );
 }
