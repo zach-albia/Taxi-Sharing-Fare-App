5d7775f304ca3c42a998fb1ca43650cb23d65a1e
Zach Albia <zach.albia@gmail.com>
2018-10-25 16:32:51 +0800

    feat(client): Write LoginForm fields using react-final-form components

--- packages/client/src/components/pages/logout/LoginForm.tsx
+++ packages/client/src/components/pages/logout/LoginForm.tsx
@@ -2,34 +2,51 @@ import Button from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Input from "@material-ui/core/Input";
-import InputLabel from "@material-ui/core/InputLabel";
+import { FinalTextField } from "pangwarta-shared/dist/lib";
+import { sleep } from "pangwarta-shared/dist/lib/utils/helpers";
 import * as React from "react";
-import { Form } from "react-final-form";
+import { Field, Form, FormRenderProps } from "react-final-form";
 import { FormProps } from "../../../@types/common";
+
+const onSubmit = async values => {
+  await sleep(0);
+  window.alert(JSON.stringify(values));
+};
 
 export class LoginForm extends React.Component<FormProps> {
   render() {
     return (
-      <Form onSubmit={undefined}>
-        {() => (
-          <form className={this.props.classes.form}>
+      <Form
+        initialValues={{ email: "", password: "" }}
+        onSubmit={onSubmit}
+        subscription={{
+          submitting: true,
+          values: true
+        }}
+      >
+        {({ handleSubmit, submitting, values }: FormRenderProps) => (
+          <form className={this.props.classes.form} onSubmit={handleSubmit}>
             <FormControl margin="normal" required={true} fullWidth={true}>
-              <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input
+              <Field
+                component={FinalTextField}
                 id="email"
                 name="email"
                 autoComplete="email"
                 autoFocus={true}
+                label="Email"
+                required={true}
+                type="text"
               />
             </FormControl>
             <FormControl margin="normal" required={true} fullWidth={true}>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input
+              <Field
+                component={FinalTextField}
+                id="password"
                 name="password"
                 type="password"
-                id="password"
                 autoComplete="current-password"
+                label="Password"
+                required={true}
               />
             </FormControl>
             <FormControlLabel
@@ -42,9 +59,11 @@ export class LoginForm extends React.Component<FormProps> {
               variant="contained"
               color="primary"
               className={this.props.classes.submit}
+              disabled={submitting}
             >
               Sign in
             </Button>
+            <pre>{JSON.stringify(values)}</pre>
           </form>
         )}
       </Form>
