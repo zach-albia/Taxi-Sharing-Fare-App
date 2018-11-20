fce2c0275663fab4c1da39b2aadc94ded15e98b7
Zach Albia <zach.albia@gmail.com>
2018-11-20 12:53:11 +0800

    feat(client): Add location chooser dialog, reword location captions

--- packages/client/src/components/pages/main/Main.tsx
+++ packages/client/src/components/pages/main/Main.tsx
@@ -3,6 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button/Button";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import red from "@material-ui/core/colors/red";
+import Dialog from "@material-ui/core/Dialog";
 import { StandardProps } from "@material-ui/core/es";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelActions from "@material-ui/core/ExpansionPanelActions";
@@ -14,12 +15,14 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import MenuItem from "@material-ui/core/MenuItem";
+import Slide from "@material-ui/core/Slide";
 import { StyleRules } from "@material-ui/core/styles";
 import withStyles from "@material-ui/core/styles/withStyles";
 import TextField from "@material-ui/core/TextField";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PersonIcon from "@material-ui/icons/PersonPin";
 import classNames from "classnames";
@@ -67,167 +70,220 @@ export interface MainProps
   classes: Record<MainClassKey, string>;
 }
 
-function Main(props: MainProps) {
-  const { classes } = props;
-  return (
-    <>
-      <AppBar>
-        <Toolbar>
-          <Link href="/">
-            <Typography
-              variant="subheading"
+interface MainState {
+  dialogOpen: boolean;
+}
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class Main extends React.Component<MainProps, MainState> {
+  state: MainState = {
+    dialogOpen: false
+  };
+
+  private openDialog = () => {
+    this.setState({ dialogOpen: true });
+  };
+
+  private closeDialog = () => {
+    this.setState({ dialogOpen: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { dialogOpen } = this.state;
+    return (
+      <>
+        <AppBar>
+          <Toolbar>
+            <Link href="/">
+              <Typography
+                variant="subheading"
+                color="inherit"
+                className={classes.grow}
+              >
+                Taxi Sharing
+              </Typography>
+            </Link>
+            <Link href="/history">
+              <Button color="inherit">Ride History</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Typography variant="subheading" gutterBottom={true}>
+          Ride Details
+        </Typography>
+        <Typography variant="caption" gutterBottom={true}>
+          Passengers
+        </Typography>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <PersonIcon
               color="inherit"
-              className={classes.grow}
-            >
-              Taxi Sharing
-            </Typography>
-          </Link>
-          <Link href="/history">
-            <Button color="inherit">Ride History</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <Typography variant="subheading" gutterBottom={true}>
-        Ride Details
-      </Typography>
-      <Typography variant="caption" gutterBottom={true}>
-        Passengers
-      </Typography>
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <PersonIcon
-            color="inherit"
-            className={classNames(classes.markerIcon, classes.aPersonIcon)}
-          />
-          <Typography className={classes.grow}>Fulan AlFulani</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <List>
-            <ListItem className={classes.listItem} component={ButtonBase}>
-              <ListItemIcon>
-                <MapMarkerIcon className={classes.markerIcon} />
-              </ListItemIcon>
-              <Typography>
-                <Typography variant="caption">Origin:</Typography>
-                University of Bahrain
-              </Typography>
-            </ListItem>
-            <ListItem className={classes.listItem} component={ButtonBase}>
-              <ListItemIcon>
-                <MapMarkerIcon className={classes.markerIcon} />
-              </ListItemIcon>
-              <Typography>
-                <Typography variant="caption">Destination:</Typography>
-                Al Kindi Specialized Hospital
-              </Typography>
-            </ListItem>
-          </List>
-        </ExpansionPanelDetails>
-        <ExpansionPanelActions>
-          <Button size="small">Edit Name</Button>
-          <Button size="small" color="primary">
-            Delete
-          </Button>
-        </ExpansionPanelActions>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <PersonIcon
-            color="inherit"
-            className={classNames(classes.markerIcon, classes.aPersonIcon)}
-          />
-          <Typography className={classes.grow}>'Illan Al'illanah</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <List>
-            <ListItem className={classes.listItem} component={ButtonBase}>
-              <ListItemIcon>
-                <MapMarkerIcon className={classes.markerIcon} />
-              </ListItemIcon>
-              <Typography>
-                <Typography variant="caption">Origin:</Typography>
-                Arabian Gulf University
-              </Typography>
-            </ListItem>
-            <ListItem className={classes.listItem} component={ButtonBase}>
-              <ListItemIcon>
-                <MapMarkerIcon className={classes.markerIcon} />
-              </ListItemIcon>
-              <Typography>
-                <Typography variant="caption">Destination:</Typography>
-                Al Abraaj
-              </Typography>
-            </ListItem>
-          </List>
-        </ExpansionPanelDetails>
-        <ExpansionPanelActions>
-          <Button size="small">Edit Name</Button>
-          <Button size="small" color="primary">
-            Delete
-          </Button>
-        </ExpansionPanelActions>
-      </ExpansionPanel>
-      <TextField
-        variant="outlined"
-        fullWidth={true}
-        placeholder="Add a passenger..."
-        margin="normal"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="start">
-              <IconButton>
-                <AddIcon />
+              className={classNames(classes.markerIcon, classes.aPersonIcon)}
+            />
+            <Typography className={classes.grow}>Fulan AlFulani</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <List>
+              <ListItem
+                className={classes.listItem}
+                component={ButtonBase}
+                onClick={this.openDialog}
+              >
+                <ListItemIcon>
+                  <MapMarkerIcon className={classes.markerIcon} />
+                </ListItemIcon>
+                <Typography>
+                  <Typography variant="caption">Pick-up Location:</Typography>
+                  University of Bahrain
+                </Typography>
+              </ListItem>
+              <ListItem className={classes.listItem} component={ButtonBase}>
+                <ListItemIcon>
+                  <MapMarkerIcon className={classes.markerIcon} />
+                </ListItemIcon>
+                <Typography>
+                  <Typography variant="caption">Drop-off Location:</Typography>
+                  Al Kindi Specialized Hospital
+                </Typography>
+              </ListItem>
+            </List>
+          </ExpansionPanelDetails>
+          <ExpansionPanelActions>
+            <Button size="small">Edit Name</Button>
+            <Button size="small" color="primary">
+              Delete
+            </Button>
+          </ExpansionPanelActions>
+        </ExpansionPanel>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <PersonIcon
+              color="inherit"
+              className={classNames(classes.markerIcon, classes.aPersonIcon)}
+            />
+            <Typography className={classes.grow}>'Illan Al'illanah</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <List>
+              <ListItem className={classes.listItem} component={ButtonBase}>
+                <ListItemIcon>
+                  <MapMarkerIcon className={classes.markerIcon} />
+                </ListItemIcon>
+                <Typography>
+                  <Typography variant="caption">Pick-up Location:</Typography>
+                  Arabian Gulf University
+                </Typography>
+              </ListItem>
+              <ListItem className={classes.listItem} component={ButtonBase}>
+                <ListItemIcon>
+                  <MapMarkerIcon className={classes.markerIcon} />
+                </ListItemIcon>
+                <Typography>
+                  <Typography variant="caption">Drop-off Location:</Typography>
+                  Al Abraaj
+                </Typography>
+              </ListItem>
+            </List>
+          </ExpansionPanelDetails>
+          <ExpansionPanelActions>
+            <Button size="small">Edit Name</Button>
+            <Button size="small" color="primary">
+              Delete
+            </Button>
+          </ExpansionPanelActions>
+        </ExpansionPanel>
+        <TextField
+          variant="outlined"
+          fullWidth={true}
+          placeholder="Add a passenger..."
+          margin="normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <AddIcon />
+                </IconButton>
+              </InputAdornment>
+            )
+          }}
+        />
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" className={classes.markerIcon}>
+                <MapMarkerIcon />
+              </InputAdornment>
+            )
+          }}
+          label="Ride Origin"
+          select={true}
+          variant="outlined"
+          fullWidth={true}
+          value={0}
+          margin="normal"
+        >
+          <MenuItem value={0}>University of Bahrain</MenuItem>
+          <MenuItem value={1}>Arabian Gulf University</MenuItem>
+        </TextField>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" className={classes.markerIcon}>
+                <MapMarkerIcon />
+              </InputAdornment>
+            )
+          }}
+          label="Ride Destination"
+          select={true}
+          variant="outlined"
+          fullWidth={true}
+          value={1}
+          margin="normal"
+        >
+          <MenuItem value={0}>Al Kindi Specialized Hospital</MenuItem>
+          <MenuItem value={1}>Al Abraaj</MenuItem>
+        </TextField>
+        <Button
+          fullWidth={true}
+          variant="contained"
+          className={classes.button}
+          color="primary"
+        >
+          Split the Fare
+        </Button>
+        <Dialog
+          fullScreen={true}
+          open={dialogOpen}
+          onClose={this.closeDialog}
+          TransitionComponent={Transition}
+        >
+          <AppBar>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                onClick={this.closeDialog}
+                aria-label="Close"
+              >
+                <CloseIcon />
               </IconButton>
-            </InputAdornment>
-          )
-        }}
-      />
-      <TextField
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start" className={classes.markerIcon}>
-              <MapMarkerIcon />
-            </InputAdornment>
-          )
-        }}
-        label="Ride Origin"
-        select={true}
-        variant="outlined"
-        fullWidth={true}
-        value={0}
-        margin="normal"
-      >
-        <MenuItem value={0}>University of Bahrain</MenuItem>
-        <MenuItem value={1}>Arabian Gulf University</MenuItem>
-      </TextField>
-      <TextField
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start" className={classes.markerIcon}>
-              <MapMarkerIcon />
-            </InputAdornment>
-          )
-        }}
-        label="Ride Destination"
-        select={true}
-        variant="outlined"
-        fullWidth={true}
-        value={1}
-        margin="normal"
-      >
-        <MenuItem value={0}>Al Kindi Specialized Hospital</MenuItem>
-        <MenuItem value={1}>Al Abraaj</MenuItem>
-      </TextField>
-      <Button
-        fullWidth={true}
-        variant="contained"
-        className={classes.button}
-        color="primary"
-      >
-        Split the Fare
-      </Button>
-    </>
-  );
+              <Typography
+                variant="subheading"
+                color="inherit"
+                className={classes.grow}
+              >
+                Choose pick-up location
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Typography variant="display4">Map Goes Here</Typography>
+        </Dialog>
+      </>
+    );
+  }
 }
 
 export default withStyles(styles)(Main);
