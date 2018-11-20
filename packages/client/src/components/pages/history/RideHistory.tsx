e103645863ccc15db32f3b2a507696ee6481d855
Zach Albia <zach.albia@gmail.com>
2018-11-20 11:47:47 +0800

    feat(client): Duplicate main toolbar in RideHistory

--- packages/client/src/components/pages/history/RideHistory.tsx
+++ packages/client/src/components/pages/history/RideHistory.tsx
@@ -1,15 +1,26 @@
 import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button/Button";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
 import * as React from "react";
 
 export default function RideHistory() {
   return (
     <AppBar>
       <Toolbar>
-        <Typography variant="subheading" color="inherit">
-          Taxi-sharing | History
-        </Typography>
+        <Link href="/">
+          <Typography
+            variant="subheading"
+            color="inherit"
+            style={{ flexGrow: 1 }}
+          >
+            Taxi Sharing
+          </Typography>
+        </Link>
+        <Link href="/history">
+          <Button color="inherit">Ride History</Button>
+        </Link>
       </Toolbar>
     </AppBar>
   );
