b4064470974b144456d018237de35f52d2e4efff
Zach Albia <zach.albia@gmail.com>
2018-11-22 21:40:56 +0800

    feat: Add edit passenger name action

--- packages/client/src/redux/actions.ts
+++ packages/client/src/redux/actions.ts
@@ -2,16 +2,17 @@ import { createAction } from "typesafe-actions";
 import GoogleMapsApi from "../@types/GoogleMapsApi";
 import actionTypes from "./actionTypes";
 
-export const googleApiLoadedAction = createAction(
-  actionTypes.GOOGLE_API_LOADED,
-  resolve => {
-    return (api: GoogleMapsApi) => resolve(api);
-  }
-);
-
 export const addPassengerAction = createAction(
   actionTypes.ADD_PASSENGER,
-  resolve => {
-    return (name: string) => resolve(name);
-  }
+  resolve => (name: string) => resolve(name)
+);
+
+export const editPassengerNameAction = createAction(
+  actionTypes.EDIT_PASSENGER_NAME,
+  resolve => (id: string, name: string) => resolve({ id, name })
+);
+
+export const googleApiLoadedAction = createAction(
+  actionTypes.GOOGLE_API_LOADED,
+  resolve => (api: GoogleMapsApi) => resolve(api)
 );
