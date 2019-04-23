65a364f0f99118c333c5b1ceb523763ece4ed7e2
Zach Albia <zach.albia@gmail.com>
2019-04-23 14:40:15 +0800

    feat: Implement RideHistory page

--- packages/client/pages/history.tsx
+++ packages/client/pages/history.tsx
@@ -1,10 +1,15 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import { withRouter } from "next/router";
 import AppContent from "pangwarta-shared/dist/lib/layout/AppContent";
 import withRoot from "pangwarta-shared/dist/lib/layout/withRoot";
 import * as React from "react";
-import RideHistory from "../src/components/pages/history/RideHistory";
 import pages from "../src/pages";
+
+const RideHistory = dynamic(
+  () => import("../src/components/pages/history/RideHistory"),
+  { ssr: false }
+);
 
 const History = () => (
   <>
--- packages/client/src/components/pages/history/RideHistory.tsx
+++ packages/client/src/components/pages/history/RideHistory.tsx
@@ -1,5 +1,40 @@
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Link from "next/link";
 import * as React from "react";
+import keys from "../../../localStorageKeys";
+import { Result } from "../../../redux/State";
 
 export default function RideHistory() {
-  return <></>;
+  const rideIds = JSON.parse(localStorage.getItem(keys.rideIds)) as string[];
+  const results = rideIds.map(
+    id => JSON.parse(localStorage.getItem(`ride-${id}`)) as Result
+  );
+  return (
+    <>
+      <Typography variant="subheading" gutterBottom={true}>
+        Rides
+      </Typography>
+      {results.length > 0 ? (
+        <Paper>
+          <List>
+            {results.map(result => (
+              <Link href={`/ride?id=${result.id}`}>
+                <ListItem button={true}>
+                  <Typography>{result.taxiRide.timestamp}</Typography>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Paper>
+      ) : (
+        <Typography>
+          Your ride history is empty. Get started by entering some rides in the{" "}
+          <Link href="/">Taxi Sharing Page</Link>.
+        </Typography>
+      )}
+    </>
+  );
 }
